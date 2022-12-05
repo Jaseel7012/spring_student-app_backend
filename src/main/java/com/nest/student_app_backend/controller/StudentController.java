2c31503d5e6776553e80bb3585779eb2e6fb1cae
jaseel <jaseeljctcs@gmail.com>
2022-12-05 14:40:19 +0530

    initial

---
+++ src/main/java/com/nest/student_app_backend/controller/StudentController.java
@@ -0,0 +1,13 @@
+package com.nest.student_app_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @GetMapping("/")
+    public String Homepage(){
+        return "welcome my java";
+    }
+
+}
---
+++ src/test/java/com/nest/student_app_backend/StudentAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.student_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
